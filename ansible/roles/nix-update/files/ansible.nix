d7b628e9b1a90790fc2ac7a6555ae1c7ace6f96e
Justin Dray <justin@dray.be>
2025-06-14 12:45:06 +1000

    Refactor ansible configuration to use systemd for ansible-pull and remove shell aliases

--- ansible/roles/nix-update/files/ansible.nix
+++ ansible/roles/nix-update/files/ansible.nix
@@ -3,20 +3,37 @@
   lib,
   pkgs,
   ...
-}: let
-  ansiblePullCmd = "${pkgs.ansible}/bin/ansible-pull -U https://github.com/justin8/home-cluster.git -d /var/lib/ansible/local -i ansible/inventory ansible/playbook.yml >> /var/log/ansible-pull.log 2>&1";
-in {
+}: {
   # Install Ansible
   environment.systemPackages = with pkgs; [
     ansible
   ];
 
-  # Set up cron jobs for ansible-pull
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "*/10 * * * * root ${ansiblePullCmd}"
-      "@reboot root ${ansiblePullCmd}"
-    ];
+  # Set up systemd service for ansible-pull
+  systemd.services.ansible-pull = {
+    description = "Run ansible-pull to update system configuration";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.ansible ];
+    
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.ansible}/bin/ansible-pull -U https://github.com/justin8/home-cluster.git -d /var/lib/ansible/local -i ansible/inventory ansible/playbook.yml";
+      StandardOutput = "append:/var/log/ansible-pull.log";
+      StandardError = "append:/var/log/ansible-pull.log";
+    };
+  };
+
+  # Timer to run ansible-pull periodically
+  systemd.timers.ansible-pull = {
+    description = "Timer for ansible-pull";
+    wantedBy = [ "timers.target" ];
+    
+    timerConfig = {
+      OnBootSec = "0";
+      OnUnitActiveSec = "10min";
+      Unit = "ansible-pull.service";
+    };
   };
 }
