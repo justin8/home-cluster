c07e0b7eb3fef37459ce85a3fd5bd3c35b4811ea
Justin Dray <justin@dray.be>
2025-06-14 16:00:54 +1000

    Change back to cron to fix issue with nixos paths in systemd units

--- ansible/roles/nix-update/files/ansible.nix
+++ ansible/roles/nix-update/files/ansible.nix
@@ -3,37 +3,19 @@
   lib,
   pkgs,
   ...
-}: {
+}: 
+let
+  ansiblePullCmd = "${pkgs.ansible}/bin/ansible-pull -U https://github.com/justin8/home-cluster.git -d /var/lib/ansible/local -i ansible/inventory ansible/playbook.yml";
+in
+{
   # Install Ansible
   environment.systemPackages = with pkgs; [
     ansible
   ];
 
-  # Set up systemd service for ansible-pull
-  systemd.services.ansible-pull = {
-    description = "Run ansible-pull to update system configuration";
-    after = [ "network-online.target" ];
-    wants = [ "network-online.target" ];
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.ansible pkgs.git pkgs.nixos-install-tools pkgs.nixos-rebuild ];
-    
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.ansible}/bin/ansible-pull -U https://github.com/justin8/home-cluster.git -d /var/lib/ansible/local -i ansible/inventory ansible/playbook.yml";
-      StandardOutput = "append:/var/log/ansible-pull.log";
-      StandardError = "append:/var/log/ansible-pull.log";
-    };
-  };
-
-  # Timer to run ansible-pull periodically
-  systemd.timers.ansible-pull = {
-    description = "Timer for ansible-pull";
-    wantedBy = [ "timers.target" ];
-    
-    timerConfig = {
-      OnBootSec = "30";
-      OnUnitActiveSec = "10min";
-      Unit = "ansible-pull.service";
-    };
-  };
+  # Add cron jobs to run ansible-pull every 10 minutes and at reboot
+  services.cron.systemCronJobs = [
+    "@reboot root sleep 30; ${ansiblePullCmd} >> /var/log/ansible-pull.log 2>&1"
+    "*/10 * * * * root ${ansiblePullCmd} >> /var/log/ansible-pull.log 2>&1"
+  ];
 }
