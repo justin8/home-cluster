5b4ca618b7090a68a7aa6a57aab5a72ac00ee0f2
Justin Dray <justin@dray.be>
2026-09-24 18:20:00 -0700

    feat(talos): configure staging VM node and floating VIP 192.168.5.19

--- shell.nix
+++ shell.nix
@@ -52,7 +52,7 @@ pkgs.mkShell {
       echo "Configuring talos..."
       cd talos
       talhelper genconfig
-      talosctl kubeconfig --talosconfig=./clusterconfig/talosconfig --force --nodes=192.168.5.20 --endpoints=192.168.5.20 clusterconfig/kubeconfig
+      talosctl kubeconfig --talosconfig=./clusterconfig/talosconfig --force --nodes=192.168.5.19 --endpoints=192.168.5.19 clusterconfig/kubeconfig
     )
   '';
 }
